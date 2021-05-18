6728f5a43a8d38149a9472c1f836ef34cbec42d9
tuliofergulha <tfergulha@gmail.com>
2021-05-18 20:55:18 -0300

    create new service functions for products and docker-compose

---
+++ src/handlers/products.js
@@ -0,0 +1,14 @@
+'use restrict'
+
+const get = (request, h) => {
+    return 'Get';
+};
+
+const save = (request, h) => {
+    return 'Save';
+};
+
+module.exports ={
+    get,
+    save
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
+'use restrict'
+
+const productsHandler = require('./handlers/products');
+
 module.exports = [
     {
         method: 'GET',
         path: '/api/v1/products',
-        handler: function (request, h) {
-    
-            return 'Hello World!';
-        }
+        handler: productsHandler.get
+    },
+    {
+        method: 'POST',
+        path: '/api/v1/products',
+        handler: productsHandler.save
     },
 ]
